af442266292440a8be194f5794bf4e1e831b5db7
bagantegianluca <bagantegianluca@gmail.com>
2024-02-04 18:04:13 +0100

    Add Chat GPT Solution

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,21 @@ export default {
     };
   },
   mounted() {
+    const axiosRequests = [];
+
     for (let i = 0; i < 10; i++) {
-      axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((response) => {
-          // success
-          console.log(response);
-          this.mailList.push(response.data.response);
-        });
+      axiosRequests.push(
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      );
     }
+
+    Promise.all(axiosRequests)
+      .then((responses) => {
+        this.mailList = responses.map((response) => response.data.response);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   },
 };
 </script>
